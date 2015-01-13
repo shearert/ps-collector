3c08a8f0fcda798f089ad953550ea1160eca6ef1
Edgar Fajardo <efajardo@physics.ucsd.edu>
2015-01-13 00:04:06 +0000

    Improved memmory footprint
    git-svn-id: https://vdt.cs.wisc.edu/svn/software/rsv-perfsonar/trunk@20426 4e558342-562e-0410-864c-e07659590f8c

--- libexec/probes/worker-scripts/esmonduploader/caller.py
+++ libexec/probes/worker-scripts/esmonduploader/caller.py
@@ -13,52 +13,26 @@ def str2bool(word):
   return word.lower() in ("true")
 
 def get_post():
-    caller.add2log("Getting data...")
+    caller.add2log("Getting data..and posting data")
     try:
         caller.getData(str2bool(opts.disp), str2bool(opts.summary))
     except Exception as err:
-        print "Error! Get unsuccessful! Exception: \"%s\" of type: \"%s\" was thrown! Quitting out." % (err,type(err))
+        print "Error! Uunsuccessful! Exception: \"%s\" of type: \"%s\" was thrown! Quitting out." % (err,type(err))
+        sys.exit(1)
     else:
-        caller.add2log("Finished getting data succesfully!")
-        caller.add2log("Starting to post data...")
-        try:
-            caller.postData(str2bool(opts.disp))
-        except Exception as err:
-            print "Error! Post unsuccessful! Exception: \"%s\" of type: \"%s\" was thrown! Quitting out." % (err,type(err))
-            sys.exit(1)
-        else:
-            caller.add2log("Finished Posting data successfully!")
-            sys.exit(0)
+        caller.add2log("Finished getting and posting data succesfully!")
+        sys.exit(0)
 
 
 def handler(signum, frame):
     caller.add2log('WARNING: Running time took more than %d seconds' % int(opts.timeout))
     sys.exit(0)
 
-# Option: Display Metadata
-#if opts.disp:
-#    try:
-#        caller.getData(opts.disp)
-#    except Exception as err:
-#        print "An error occurred! Exception:  \"%s\" of type: \"%s\" was thrown!" % (err, type(err))
-
 # Option: Get and Post Metadata
 if opts.post:
-    # Option: Loop Process (Repeat every 12 hours)
-    if opts.loop:    
-        while True:
-            get_post()
-            print "Waiting 43200 seconds (12 hours)"
-            sleep(43200)
-    # Else do once and quit out
-    else:
-        # Implementing some timeout
-        signal.signal(signal.SIGALRM, handler)
-        signal.alarm(int(opts.timeout))
-        get_post()
-        signal.alarm(0)
+   # Implementing some timeout
+    signal.signal(signal.SIGALRM, handler)
+    signal.alarm(int(opts.timeout))
+    get_post()
+    signal.alarm(0)
 
-# Option: Error Checking (Get/Post without error catching)
-if opts.err:
-    caller.getData()
-    caller.postData()
--- libexec/probes/worker-scripts/esmonduploader/esmonduploader.py
+++ libexec/probes/worker-scripts/esmonduploader/esmonduploader.py
@@ -97,119 +97,91 @@ class EsmondUploader(object):
             self.add2log("Reading Summaries")
         else:
             self.add2log("Omiting Sumaries")
-        i = 0
         for md in self.conn.get_metadata():
             # Check for repeat data
             if md.metadata_key in self.old_list:
                 continue
             else:
+                # It is a new metadata
+                # Building the arguments for the post
+                args = {
+                    "subject_type": md.subject_type,
+                    "source": md.source,
+                    "destination": md.destination,
+                    "tool_name": md.tool_name,
+                    "measurement_agent": md.measurement_agent,
+                    "input_source": self.connect,
+                    "input_destination": self.goc
+                }
+                if not md.time_duration is None:
+                    args["time_duration"] = md.time_duration
                 # Assigning each metadata object property to class variables
-                self.destination.append(md.destination)
-                self.input_destination.append(md.input_destination)
-                self.input_source.append(md.input_source)
-                self.measurement_agent.append(md.measurement_agent)
-                self.source.append(md.source)
-                self.subject_type.append(md.subject_type)
-                self.time_duration.append(md.time_duration)
-                self.tool_name.append(md.tool_name)
-                self.event_types.append(md.event_types)
-                self.metadata_key.append(md.metadata_key)
+                event_types = md.event_types
+                metadata_key = md.metadata_key
                 # print extra debugging only if requested
-                self.add2log("Reading New METADATA/DATA %d" % (i+1))
+                self.add2log("Reading New METADATA/DATA %s" % (md.metadata_key))
                 if disp:
-                    print "Destination: " + self.destination[i]
-                    print "Event Types: " + str(self.event_types[i])
-                    print "Input Destination: " + self.input_destination[i]
-                    print "Input Source: " + self.input_source[i]
-                    print "Measurement Agent: " + self.measurement_agent[i]
-                    print "Source: " + self.source[i]
-                    print "Subject_type: " + self.subject_type[i]
-                    if not self.time_duration[i] is None:
-                        print "Time Duration: " + self.time_duration[i]
-                    print "Tool Name: " + self.tool_name[i]
-                    print "Metadata Key: " + self.metadata_key[i] + " /n/n"
+                    print args
                 # Get Events and Data Payload
                 # temp_list holds the sumaries for all event types for metadata i
-                temp_list = [] 
-                # temp_list3 is a list of lists. 
+                summaries = [] 
+                # datapoints is a dict of lists
                 # Each of its members are lists of datapoints of a given event_type
-                temp_list3 = []
+                datapoints = {}
                 # et = event type
-                for eventype in self.event_types[i]: 
-                    # temp_list2 is for adding all data points of a same event type
-                    temp_list2 = []
+                for eventype in event_types: 
+                    datapoints[eventype] = []
                     et = md.get_event_type(eventype)
                     if summary:
-                        temp_list.append(et.summaries)
+                        summaries.append(et.summaries)
                     else:
-                        temp_list.append([])
+                        summaries.append([])
                     # Skip readind data points for certain event types to improv efficiency  
                     if eventype not in allowedEvents:                                                                                                       
                     #if eventype in skipEvents:
                         #self.add2log("Skipped reading data for event type: %s for metadatda %d" % (eventype, i+1))
-                        temp_list3.append(temp_list2)
                         continue
                     dpay = et.get_data()
                     for dp in dpay.data:
                         tup = (dp.ts_epoch, dp.val)
-                        temp_list2.append(tup)
-                    temp_list3.append(temp_list2)
-                self.datapoint.append(temp_list3)
-                self.summaries.append(temp_list)
-            i += 1
+                        datapoints[eventype].append(tup)
+            self.postData2(args, event_types, summaries, metadata_key, datapoints, disp)
 
-    # Post Data
-    def postData(self, disp=False):
-        for i in range(len(self.destination)):
-            # Looping through metadata
-            args = {
-                "subject_type": self.subject_type[i],
-                "source": self.source[i],
-                "destination": self.destination[i],
-                "tool_name": self.tool_name[i],
-                "measurement_agent": self.measurement_agent[i],
-                "input_source": self.connect,
-                "input_destination": self.goc
-            }
-            # Time duration may not always be set
-            if not self.time_duration[i] is None:
-                    args["time_duration"] = self.time_duration[i]        
-            mp = MetadataPost(self.goc, username=self.username, api_key=self.key, **args)
-            # Posting Event Types and Summaries
-            for event_type, summary in zip(self.event_types[i], self.summaries[i]):
-                mp.add_event_type(event_type)
-                if summary:
-                    mp.add_summary_type(event_type, summary[0][0], summary[0][1])
-            self.add2log("posting NEW METADATA/DATA %d" % (i+1))
-            if disp:
-                print self.metadata_key[i]
-            new_meta = mp.post_metadata()
-            # Catching bad posts
-            try:
-                new_meta.metadata_key
-            except:
-                print 'ERROR'
-                print args
-                print self.event_types[i]
-                print self.summaries[i]
-                continue
-            self.add2log("Finished posting summaries and metadata %d" % (i+1))
-            # Posting Data Points
-            et = EventTypeBulkPost(self.goc, username=self.username, api_key=self.key, metadata_key=new_meta.metadata_key)
-            for event_num in range(len(self.event_types[i])):
-                # datapoints are tuples the first field is epoc the second the value
-                for datapoint in self.datapoint[i][event_num]:
-                    # packet-loss-rate is read as a float but should be uploaded as a dict with denominator and numerator 
-                    if 'packet-loss-rate' in self.event_types[i][event_num]:
-                        if isinstance(datapoint[1], float):
-                            packetLossFraction = Fraction(datapoint[1]).limit_denominator(300)
-                            et.add_data_point(self.event_types[i][event_num], datapoint[0], {'denominator':  packetLossFraction.denominator, \
+
+    def postData2(self, args, event_types, summaries, metadata_key, datapoints, disp=False):
+        mp = MetadataPost(self.goc, username=self.username, api_key=self.key, **args)
+        for event_type, summary in zip(self.event_types, self.summaries):
+            mp.add_event_type(event_type)
+            if summary:
+                mp.add_summary_type(event_type, summary[0][0], summary[0][1])
+        self.add2log("posting NEW METADATA/DATA")
+        new_meta = mp.post_metadata()
+        # Catching bad posts                                                                                                                              
+        try:
+            new_meta.metadata_key
+        except:
+            print 'ERROR'
+            print args
+            print event_types
+            print summaries
+            return
+        self.add2log("Finished posting summaries and metadata")
+        et = EventTypeBulkPost(self.goc, username=self.username, api_key=self.key, metadata_key=new_meta.metadata_key)
+        for event_type in event_types:
+            # datapoints are tuples the first field is epoc the second the value                                                                          
+            for datapoint in datapoints[event_type]:
+                # packet-loss-rate is read as a float but should be uploaded as a dict with denominator and numerator                                     
+                if 'packet-loss-rate' in event_type:
+                    if isinstance(datapoint[1], float):
+                        packetLossFraction = Fraction(datapoint[1]).limit_denominator(300)
+                        et.add_data_point(event_type, datapoint[0], {'denominator':  packetLossFraction.denominator, \
                                                                              'numerator': packetLossFraction.numerator})
-                        else:
-                            self.add2log("weird packet loss rate")
-                    # For the rests the data points are uploaded as they are read
                     else:
-                        et.add_data_point(self.event_types[i][event_num], datapoint[0], datapoint[1])
-            # Posting the data once all data points on the same metadata have been added
-            et.post_data()
-            self.add2log("Finish posting data for metadata %d" % (i+1))
+                        self.add2log("weird packet loss rate")
+                    # For the rests the data points are uploaded as they are read                                                                            
+                else:
+                    et.add_data_point(event_type, datapoint[0], datapoint[1])
+        # Posting the data once all data points on the same metadata have been added                                                                     
+        et.post_data()
+        self.add2log("Finish posting data for metadata")
+
