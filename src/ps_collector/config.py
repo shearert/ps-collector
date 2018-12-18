ff74a1e5827fe3d6894c05f4936fa8a86e78083a
Brian Bockelman <bbockelm@cse.unl.edu>
2018-12-18 11:03:06 -0600

    Add ability to read files in a directory.

--- src/ps_collector/config.py
+++ src/ps_collector/config.py
@@ -8,5 +8,13 @@ def get_config():
     cp = ConfigParser.ConfigParser()
     config_file = os.environ.get("PS_COLLECTOR_CONFIG", "/etc/ps-collector/config.ini")
     cp.read(config_file)
+    if cp.has_section("General") and cp.has_option("General", "config_directory"):
+        config_dir = cp.get("General", "config_directory")
+        file_list = list(os.listdir(config_dir))
+        file_list.sort()
+        for fname in file_list:
+            if fname.startswith(".") or fname.endswith(".rpmsave") or fname.endswith(".rpmnew"):
+                continue
+            cp.read(os.path.join(config_dir, fname))
     return cp
 
