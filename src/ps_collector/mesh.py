574709794cb869ce4ff76120922e94bfdd9065e3
Derek Weitzel <djw8605@gmail.com>
2019-03-08 12:34:29 -0600

    Read mesh URL's from the read_url

--- src/ps_collector/mesh.py
+++ src/ps_collector/mesh.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 
 import requests
 import json
+from urlparse import urlparse
 
 
 class Mesh:
@@ -28,18 +29,18 @@ class Mesh:
         Download the nodes from a single mesh
         """
         nodes = set()
-        
+
         response = requests.get(mesh_url)
         response_json = response.json()
-        if 'tests' not in response_json:
-            return []
-        
-        for test in response_json['tests']:
-            try:
-                nodes.update(test['members']['members'])
-            except KeyError as ke:
-                print("Failed to load nodes from mesh url: %s and test: %s" % (mesh_url, test['members']['type']))
 
+        for org in response_json.get('organizations', []):
+            for site in org.get('sites', []):
+                for host in site.get('hosts', []):
+                    for url in host.get('measurement_archives', []):
+                        parsed = urlparse(url['read_url'])
+                        print(parsed.netloc)
+                        nodes.update([parsed.netloc])
+                        print(url['read_url'])
         return nodes
 
     def _download_toplevel(self):
@@ -51,5 +52,10 @@ class Mesh:
         for sub_mesh in response.json():
             to_return.append(sub_mesh['include'][0])
         return to_return
+
+
+if __name__ == "__main__":
+    mesh = Mesh()
+    print(mesh.get_nodes())
 
 
