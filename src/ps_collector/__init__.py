80336cc6decbc64dcf41bb412784d37b4db7f9eb
shearert <shearert@umich.edu>
2022-04-29 08:49:00 -0400

    Update __init__.py

--- src/ps_collector/__init__.py
+++ src/ps_collector/__init__.py
@@ -1,11 +1,11 @@
 
-from . import sharedrabbitmq
+# from . import sharedrabbitmq
 
-# Shared RabbitMQ
-shared_rabbitmq = None
+# # Shared RabbitMQ
+# shared_rabbitmq = None
 
-def get_rabbitmq_connection(cp):
-    global shared_rabbitmq
-    if shared_rabbitmq == None:
-        shared_rabbitmq = sharedrabbitmq.SharedRabbitMQ(cp)
-    return shared_rabbitmq
+# def get_rabbitmq_connection(cp):
+#     global shared_rabbitmq
+#     if shared_rabbitmq == None:
+#         shared_rabbitmq = sharedrabbitmq.SharedRabbitMQ(cp)
+#     return shared_rabbitmq
